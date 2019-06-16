4981a2877f8cf7e6aac63ea979648780270b953c
Nick Magerko <nmagerko@gmail.com>
2019-06-15 21:36:55 -0500

    Use structopt to reduce commandline boilerplate

--- kvs/src/bin/kvs.rs
+++ kvs/src/bin/kvs.rs
@@ -1,46 +1,43 @@
-extern crate clap;
-use clap::{App, Arg, SubCommand};
+extern crate structopt;
+
 use kvs::KvStore;
 use std::process;
+use structopt::StructOpt;
 
 fn main() {
-    let matches = App::new(clap::crate_name!())
-        .version(clap::crate_version!())
-        .author(clap::crate_authors!(", "))
-        .about("A key-value store written in Rust")
-        .subcommand(SubCommand::with_name("get").arg(Arg::with_name("key").required(true)))
-        .subcommand(
-            SubCommand::with_name("set")
-                .arg(Arg::with_name("key").required(true))
-                .arg(Arg::with_name("value").required(true)),
-        )
-        .subcommand(SubCommand::with_name("rm").arg(Arg::with_name("key").required(true)))
-        .get_matches();
-
+    let opts = Opts::from_args();
     let mut store = KvStore::new();
 
-    match matches.subcommand() {
-        ("get", Some(matches)) => {
-            let key = matches.value_of("key").unwrap().to_string();
+    match opts.cmd {
+        Command::Get { key } => {
             let _value = store.get(key);
             print_unimplemented();
         }
-        ("set", Some(matches)) => {
-            let key = matches.value_of("key").unwrap().to_string();
-            let value = matches.value_of("value").unwrap().to_string();
+        Command::Set { key, value } => {
             store.set(key, value);
             print_unimplemented();
         }
-        ("rm", Some(matches)) => {
-            let key = matches.value_of("key").unwrap().to_string();
+        Command::Remove { key } => {
             store.remove(key);
             print_unimplemented();
         }
-        _ => {
-            eprintln!("Invalid usage: no such subcommand");
-            process::exit(1);
-        }
     }
+}
+
+#[derive(StructOpt)]
+struct Opts {
+    #[structopt(subcommand)]
+    cmd: Command,
+}
+
+#[derive(StructOpt)]
+enum Command {
+    #[structopt(name = "get")]
+    Get { key: String },
+    #[structopt(name = "set")]
+    Set { key: String, value: String },
+    #[structopt(name = "rm")]
+    Remove { key: String },
 }
 
 fn print_unimplemented() {
